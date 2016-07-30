8158e86a0e256e5335d4fd536efd3e120be14d67
Dmitry Lebedev <p17dmitry@gmail.com>
2016-07-30 09:32:17 +0000

    3. Find replace console.log with throw

--- solution.js
+++ solution.js
@@ -1,10 +1,10 @@
 var mongo = require('mongodb').MongoClient
 mongo.connect('mongodb://localhost:27017/learnyoumongo', function(err, db) {
-    if (err) return console.error(err)
+    if (err) throw err
     db.collection('parrots').find({
         age: { $gt: +process.argv[2] }
     }).toArray(function(err, docs) {
-        if (err) return console.error(err)
+        if (err) throw err
         console.log(docs)
     })
     db.close();
