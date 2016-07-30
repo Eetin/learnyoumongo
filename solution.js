c4d261e115f8a203d37f56bdcedf24c062450f9c
Dmitry Lebedev <p17dmitry@gmail.com>
2016-07-30 10:00:54 +0000

    7. Remove

--- solution.js
+++ solution.js
@@ -1,13 +1,9 @@
 var mongo = require('mongodb').MongoClient
 mongo.connect('mongodb://localhost:27017/' + process.argv[2], function(err, db) {
     if (err) throw err
-    var docs = db.collection('users')
-    docs.update({
-        username: 'tinatime'
-    }, {
-        $set: {
-            age: 40
-        }
+    var docs = db.collection(process.argv[3])
+    docs.remove({
+        _id: process.argv[4]
     }, function(err, data) {
         if (err) throw err
     })
