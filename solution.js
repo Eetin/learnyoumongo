e95af4bf334d11021c07e9b53e5026bd9b2a5b4f
Dmitry Lebedev <p17dmitry@gmail.com>
2016-07-30 10:15:29 +0000

    9. Aggregate

--- solution.js
+++ solution.js
@@ -1,12 +1,13 @@
 var mongo = require('mongodb').MongoClient
 mongo.connect('mongodb://localhost:27017/learnyoumongo', function(err, db) {
     if (err) throw err
-    var docs = db.collection('parrots')
-    docs.count({
-        age: { $gt: +process.argv[2] }
-    }, function(err, count) {
+    var docs = db.collection('prices')
+    var match = { $match: { size: process.argv[2] } }
+    var group = { $group: { _id: 'average', average: { $avg: '$price' } } }
+    docs.aggregate([match, group]).toArray(function(err, results) {
         if (err) throw err
-        console.log(count)
+        if (!results.length) throw new Error('No results found')
+        console.log(Number(results[0].average).toFixed(2))
     })
     db.close();
 })
