156218eec895985ee5cb3d51b5861b5bc40d63a0
Dmitry Lebedev <p17dmitry@gmail.com>
2016-07-30 09:37:34 +0000

    4. Find Project

--- solution.js
+++ solution.js
@@ -3,6 +3,8 @@ mongo.connect('mongodb://localhost:27017/learnyoumongo', function(err, db) {
     if (err) throw err
     db.collection('parrots').find({
         age: { $gt: +process.argv[2] }
+    }, {
+        _id: 0
     }).toArray(function(err, docs) {
         if (err) throw err
         console.log(docs)
