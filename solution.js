6735b58b3b064f0246a89d312786ad984569ac98
Dmitry Lebedev <p17dmitry@gmail.com>
2016-07-30 10:03:42 +0000

    8. Count

--- solution.js
+++ solution.js
@@ -1,11 +1,12 @@
 var mongo = require('mongodb').MongoClient
-mongo.connect('mongodb://localhost:27017/' + process.argv[2], function(err, db) {
+mongo.connect('mongodb://localhost:27017/learnyoumongo', function(err, db) {
     if (err) throw err
-    var docs = db.collection(process.argv[3])
-    docs.remove({
-        _id: process.argv[4]
-    }, function(err, data) {
+    var docs = db.collection('parrots')
+    docs.count({
+        age: { $gt: +process.argv[2] }
+    }, function(err, count) {
         if (err) throw err
+        console.log(count)
     })
     db.close();
 })
